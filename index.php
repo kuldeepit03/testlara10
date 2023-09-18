7ef7051dfdcfca71723406196a67404c42a94252
Kuldeep <kuldeep.it03@gmail.com>
2023-09-18 20:06:27 +0530

    change in index

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
-
 define('LARAVEL_START', microtime(true));
 
 /*
@@ -45,11 +44,8 @@ require __DIR__.'/vendor/autoload.php';
 */
 
 $app = require_once __DIR__.'/bootstrap/app.php';
-
 $kernel = $app->make(Kernel::class);
-
 $response = $kernel->handle(
     $request = Request::capture()
 )->send();
-
 $kernel->terminate($request, $response);
